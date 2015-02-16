40f2e90b2e3ce148d25ac58c8b1d90bb5972979d
Olaf Lessenich <lessenic@fim.uni-passau.de>
2015-02-16 17:55:35 +0100

    Updated contributor lines and copyright information.

--- src/de/fosd/jdime/matcher/ordered/package-info.java
+++ src/de/fosd/jdime/matcher/ordered/package-info.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
- * Copyright (C) 2013-2015 Olaf Lessenich.
+ * Copyright (C) 2013-2014 Olaf Lessenich
+ * Copyright (C) 2014-2015 University of Passau, Germany
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
