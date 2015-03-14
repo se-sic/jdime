aeaaad63977a4b0ba9e91e59eb729d784adbe143
Georg Seibt <seibt@fim.uni-passau.de>
2015-03-14 19:54:08 +0100

    simple OrderedMatcher implementation using the BalancedSequence

---
+++ src/de/fosd/jdime/matcher/ordered/mceSubtree/MCESubtreeMatcher.java
@@ -0,0 +1,45 @@
+package de.fosd.jdime.matcher.ordered.mceSubtree;
+
+import de.fosd.jdime.common.Artifact;
+import de.fosd.jdime.common.MergeContext;
+import de.fosd.jdime.matcher.Matcher;
+import de.fosd.jdime.matcher.Matching;
+import de.fosd.jdime.matcher.ordered.OrderedMatcher;
+
+/**
+ * A <code>OrderedMatcher</code> that uses the <code>BalancedSequence</code> class to match <code>Artifact</code>s.
+ * Its {@link this#match(MergeContext, Artifact, Artifact, int)} method assumes that the given <code>Artifact</code>s
+ * may be interpreted as ordered trees whose nodes are labeled via their {@link Artifact#matches(Artifact)} method.
+ * 
+ * @param <T>
+ *         the type of the <code>Artifact</code>s
+ */
+public class MCESubtreeMatcher<T extends Artifact<T>> extends OrderedMatcher<T> {
+
+    /**
+     * Constructs a new <code>OrderedMatcher</code>
+     *
+     * @param matcher
+     *         matcher
+     */
+    public MCESubtreeMatcher(Matcher<T> matcher) {
+        super(matcher);
+    }
+
+    @Override
+    public Matching<T> match(MergeContext context, T left, T right, int lookAhead) {
+        BalancedSequence<T> s;
+        BalancedSequence<T> t;
+        
+        if (lookAhead == MergeContext.LOOKAHEAD_FULL) {
+            s = new BalancedSequence<>(left);
+            t = new BalancedSequence<>(right);
+        } else {
+            s = new BalancedSequence<>(left, lookAhead);
+            t = new BalancedSequence<>(right, lookAhead);
+        }
+        
+        // TODO the child matchings are missing, how are they constructed?
+        return new Matching<>(left, right, BalancedSequence.lcs(s, t) + 1);
+    }
+}
