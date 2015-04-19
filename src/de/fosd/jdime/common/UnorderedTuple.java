883cb73c8146edd6ac70856f76484941350d7adb
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-19 19:11:07 +0200

    An unordered Pair of two objects for storing matchings

---
+++ src/de/fosd/jdime/common/UnorderedTuple.java
@@ -0,0 +1,81 @@
+package de.fosd.jdime.common;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+/**
+ * A <code>Tuple</code> whose {@link #equals(Object)} and {@link #hashCode()} methods are implemented for unordered
+ * value equality.
+ *
+ * @author Georg Seibt
+ *
+ * @param <X>
+ * 		the type of the first object
+ * @param <Y>
+ * 		the type of the second object
+ */
+public class UnorderedTuple<X, Y> extends Tuple<X, Y> {
+
+	/**
+	 * Constructs an <code>UnorderedTuple</code> of the two given objects.
+	 *
+	 * @param x
+	 * 		the first object
+	 * @param y
+	 * 		the second object
+	 * @param <X>
+	 * 		the type of the first object
+	 * @param <Y>
+	 * 		the type of the second object
+	 * @return an <code>UnorderedTuple</code> containing <code>x</code> and <code>y</code>
+	 */
+	public static <X, Y> UnorderedTuple<X, Y> of(X x, Y y) {
+		return new UnorderedTuple<>(x, y);
+	}
+
+	/**
+	 * Constructs a new <code>UnorderedTuple</code> containing the given objects.
+	 *
+	 * @param x
+	 * 		the first object
+	 * @param y
+	 * 		the second object
+	 */
+	private UnorderedTuple(X x, Y y) {
+		super(x, y);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Tuple<?, ?> tuple = (Tuple<?, ?>) o;
+
+		EqualsBuilder eqBuilder = new EqualsBuilder();
+		eqBuilder.append(x, tuple.x);
+		eqBuilder.append(y, tuple.y);
+
+		if (eqBuilder.isEquals()) {
+			return true;
+		}
+
+		eqBuilder.reset();
+		eqBuilder.append(x, tuple.y);
+		eqBuilder.append(y, tuple.x);
+
+		return eqBuilder.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		int hashX = x == null ? 0 : x.hashCode();
+		int hashY = y == null ? 0 : y.hashCode();
+
+		return hashX ^ hashY;
+	}
+}
