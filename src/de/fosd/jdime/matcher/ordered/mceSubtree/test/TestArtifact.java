277bf38e09fdfd0314963007c1fe472a720245f1
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-22 18:18:35 +0200

    implementing a TestArtifact to test MCESubtreeMatcher

---
+++ src/de/fosd/jdime/matcher/ordered/mceSubtree/test/TestArtifact.java
@@ -0,0 +1,115 @@
+package de.fosd.jdime.matcher.ordered.mceSubtree.test;
+
+import de.fosd.jdime.common.Artifact;
+import de.fosd.jdime.common.MergeContext;
+import de.fosd.jdime.common.operations.MergeOperation;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * A simple <code>Artifact</code> to test the functionality of <code>MCESubtreeMatcher</code>. Not all all methods
+ * are implemented. Should be deleted later on.
+ */
+public class TestArtifact extends Artifact<TestArtifact> {
+
+	@Override
+	public TestArtifact addChild(TestArtifact child) throws IOException {
+		return null;
+	}
+
+	@Override
+	public void copyArtifact(TestArtifact destination) throws IOException {
+
+	}
+
+	@Override
+	public void createArtifact(boolean isLeaf) throws IOException {
+
+	}
+
+	@Override
+	public TestArtifact createConflictDummy(TestArtifact type, TestArtifact left, TestArtifact right) throws FileNotFoundException {
+		return null;
+	}
+
+	@Override
+	public TestArtifact createEmptyDummy() throws FileNotFoundException {
+		return null;
+	}
+
+	@Override
+	protected String dumpTree(String indent) {
+		return null;
+	}
+
+	@Override
+	public boolean exists() {
+		return false;
+	}
+
+	@Override
+	public String getId() {
+		return null;
+	}
+
+	@Override
+	public String getStatsKey(MergeContext context) {
+		return null;
+	}
+
+	@Override
+	public int hashCode() {
+		return 0;
+	}
+
+	@Override
+	public boolean hasUniqueLabels() {
+		return false;
+	}
+
+	@Override
+	public void initializeChildren() {
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public boolean isLeaf() {
+		return false;
+	}
+
+	@Override
+	public boolean isOrdered() {
+		return false;
+	}
+
+	@Override
+	public boolean matches(TestArtifact other) {
+		return false;
+	}
+
+	@Override
+	public void merge(MergeOperation<TestArtifact> operation, MergeContext context) throws IOException, InterruptedException {
+
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+
+	@Override
+	public void write(String str) throws IOException {
+
+	}
+
+	@Override
+	public int compareTo(TestArtifact o) {
+		return 0;
+	}
+}
