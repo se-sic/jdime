3589cf26f679876c5622e77c6e258fd9f96e895c
Georg Seibt <seibt@fim.uni-passau.de>
2015-05-06 22:05:19 +0200

    file choosing
    (cherry picked from commit 5cac7e2236ee8fc120af3f10bdd9090140d1d8f1)
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

--- src/de/fosd/jdime/gui/GUI.java
+++ src/de/fosd/jdime/gui/GUI.java
@@ -1,12 +1,18 @@
 package de.fosd.jdime.gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.File;
 
 public class GUI extends Application {
 
@@ -18,6 +24,11 @@ public class GUI extends Application {
 	public TextField right;
 	public TextField jdime;
 	public TextField cmdArgs;
+
+	private File leftArtifact;
+	private File baseArtifact;
+	private File rightArtifact;
+	private File jDimeBinary;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -33,20 +44,36 @@ public class GUI extends Application {
 		primaryStage.show();
 	}
 
-	public void chooseLeft() {
+	public void chooseLeft(ActionEvent event) {
+		FileChooser chooser = new FileChooser();
+		Window window = ((Node) event.getTarget()).getScene().getWindow();
 
+		leftArtifact = chooser.showOpenDialog(window);
+		left.setText(leftArtifact.getAbsolutePath());
 	}
 
-	public void chooseBase() {
+	public void chooseBase(ActionEvent event) {
+		FileChooser chooser = new FileChooser();
+		Window window = ((Node) event.getTarget()).getScene().getWindow();
 
+		baseArtifact = chooser.showOpenDialog(window);
+		base.setText(baseArtifact.getAbsolutePath());
 	}
 
-	public void chooseRight() {
+	public void chooseRight(ActionEvent event) {
+		FileChooser chooser = new FileChooser();
+		Window window = ((Node) event.getTarget()).getScene().getWindow();
 
+		rightArtifact = chooser.showOpenDialog(window);
+		right.setText(rightArtifact.getAbsolutePath());
 	}
 
-	public void chooseJDime() {
+	public void chooseJDime(ActionEvent event) {
+		FileChooser chooser = new FileChooser();
+		Window window = ((Node) event.getTarget()).getScene().getWindow();
 
+		jDimeBinary = chooser.showOpenDialog(window);
+		jdime.setText(jDimeBinary.getAbsolutePath());
 	}
 
 	public void runClicked() {
