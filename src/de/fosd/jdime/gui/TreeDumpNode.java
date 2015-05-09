1784a210b18ff32cb9e93c45e3375d6b12f16ac9
Georg Seibt <seibt@fim.uni-passau.de>
2015-05-09 21:52:50 +0200

    use String as the id (instead of int), add fillColor property to the bean
    (cherry picked from commit 7251d7c95183f1aa3d55ecc62135092c29c6cff5)
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

--- src/de/fosd/jdime/gui/TreeDumpNode.java
+++ src/de/fosd/jdime/gui/TreeDumpNode.java
@@ -1,41 +1,107 @@
 package de.fosd.jdime.gui;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-public class TreeDumpNode {
+/**
+ * A bean class encapsulating parameters parsed from a Graphviz node declaration.
+ */
+class TreeDumpNode {
 
-	private IntegerProperty id;
-	private StringProperty astType;
+	private StringProperty id;
+	private StringProperty label;
+	private StringProperty fillColor;
 
-	public TreeDumpNode(int id, String astType) {
-		this.id = new SimpleIntegerProperty(id);
-		this.astType = new SimpleStringProperty(astType);
+	/**
+	 * Creates a new <code>TreeDumpNode</code> encapsulating the given <code>id</code> and <code>astType</code>.
+	 *
+	 * @param id the ID of the node
+	 * @param label the label of the node
+	 */
+	public TreeDumpNode(String id, String label) {
+		this.id = new SimpleStringProperty(id);
+		this.label = new SimpleStringProperty(label);
+		this.fillColor = new SimpleStringProperty();
 	}
 
-	public int getId() {
+	/**
+	 * Returns the id.
+	 *
+	 * @return the id
+	 */
+	public String getId() {
 		return id.get();
 	}
 
-	public IntegerProperty idProperty() {
+	/**
+	 * Returns the ID property.
+	 *
+	 * @return the ID <code>StringProperty</code>
+	 */
+	public StringProperty idProperty() {
 		return id;
 	}
 
-	public void setId(int id) {
+	/**
+	 * Sets id to the given value.
+	 *
+	 * @param id the new id
+	 */
+	public void setId(String id) {
 		this.id.set(id);
 	}
 
-	public String getAstType() {
-		return astType.get();
+	/**
+	 * Returns the label.
+	 *
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label.get();
 	}
 
-	public StringProperty astTypeProperty() {
-		return astType;
+	/**
+	 * Returns the label property.
+	 *
+	 * @return the label <code>StringProperty</code>
+	 */
+	public StringProperty labelProperty() {
+		return label;
 	}
 
-	public void setAstType(String astType) {
-		this.astType.set(astType);
+	/**
+	 * Sets label to the given value.
+	 *
+	 * @param label the new label
+	 */
+	public void setLabel(String label) {
+		this.label.set(label);
+	}
+
+	/**
+	 * Returns the fill color.
+	 *
+	 * @return the fill color
+	 */
+	public String getFillColor() {
+		return fillColor.get();
+	}
+
+	/**
+	 * Returns the fill color property.
+	 *
+	 * @return the fill color <code>StringProperty</code>
+	 */
+	public StringProperty fillColorProperty() {
+		return fillColor;
+	}
+
+	/**
+	 * Sets fill color to the given value.
+	 *
+	 * @param fillColor the new fill color
+	 */
+	public void setFillColor(String fillColor) {
+		this.fillColor.set(fillColor);
 	}
 }
