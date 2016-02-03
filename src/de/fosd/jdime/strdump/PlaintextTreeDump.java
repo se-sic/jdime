7a892181ce115fa2cc3d6a48d0260763b7bcc03c
Georg Seibt <seibt@fim.uni-passau.de>
2016-02-03 13:54:08 +0100

    remove old code

--- src/de/fosd/jdime/strdump/PlaintextTreeDump.java
+++ src/de/fosd/jdime/strdump/PlaintextTreeDump.java
@@ -25,7 +25,6 @@ package de.fosd.jdime.strdump;
 
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 
 import de.fosd.jdime.common.Artifact;
@@ -39,106 +38,6 @@ import de.fosd.jdime.matcher.Matching;
 public class PlaintextTreeDump implements StringDumper {
 
     private static final String LS = System.lineSeparator();
-
-    /**
-     * Appends the plain-text tree representation of the given <code>artifact</code> and its children to the
-     * <code>builder</code>.
-     *
-     * @param artifact
-     *         the <code>Artifact</code> to dump
-     * @param indent
-     *         the indentation for the current level
-     */
-    private <T extends Artifact<T>> void dumpTree(StringBuilder builder, Artifact<T> artifact,
-                                                  Function<Artifact<T>, String> getLabel, String indent) {
-
-        String ls = System.lineSeparator();
-        Matching<T> m = null;
-
-        if (!artifact.isConflict() && artifact.hasMatches()) {
-            String color = "";
-
-            for (Map.Entry<Revision, Matching<T>> entry : artifact.getMatches().entrySet()) {
-                m = entry.getValue();
-                color = m.getHighlightColor().toShell();
-            }
-
-            builder.append(color);
-        }
-
-        if (artifact.isConflict()) {
-            builder.append(Color.RED.toShell());
-            builder.append(indent).append("(").append(artifact.getId()).append(") ");
-            builder.append(getLabel.apply(artifact));
-            builder.append(ls);
-            builder.append(Color.RED.toShell());
-            builder.append("<<<<<<< ");
-            builder.append(ls);
-
-            T left = artifact.getLeft();
-            T right = artifact.getRight();
-
-            // children
-            if (left != null) {
-                dumpTree(builder, left, getLabel, indent);
-            }
-
-            builder.append(Color.RED.toShell());
-            builder.append("======= ");
-            builder.append(ls);
-
-            // children
-            if (right != null) {
-                dumpTree(builder, right, getLabel, indent);
-            }
-
-            builder.append(Color.RED.toShell());
-            builder.append(">>>>>>> ");
-            builder.append(Color.DEFAULT.toShell());
-            builder.append(ls);
-        } else if (artifact.isChoice()) {
-            Set<String> conditions = artifact.getVariants().keySet();
-
-            builder.append(Color.RED.toShell());
-            builder.append(indent).append("(").append(artifact.getId()).append(") ");
-            builder.append(getLabel.apply(artifact));
-            builder.append(ls);
-
-            for (String condition : conditions) {
-                builder.append(Color.RED.toShell());
-                builder.append("#ifdef ").append(condition);
-                builder.append(ls);
-
-                // children
-                T variant = artifact.getVariants().get(condition);
-
-                if (variant != null) {
-                    dumpTree(builder, variant, getLabel, indent);
-                }
-
-                builder.append(Color.RED.toShell());
-                builder.append("#endif");
-                builder.append(Color.DEFAULT.toShell());
-                builder.append(ls);
-
-            }
-        } else {
-            builder.append(indent).append("(").append(artifact.getId()).append(") ");
-            builder.append(getLabel.apply(artifact));
-
-            if (artifact.hasMatches()) {
-                builder.append(" <=> (").append(m.getMatchingArtifact(artifact).getId()).append(")");
-                builder.append(Color.DEFAULT.toShell());
-            }
-
-            builder.append(ls);
-
-            // children
-            for (T child : artifact.getChildren()) {
-                dumpTree(builder, child, getLabel, indent + "  ");
-            }
-        }
-    }
 
     /**
      * Appends a plaintext representation of the tree with <code>artifact</code> at its root to the given
