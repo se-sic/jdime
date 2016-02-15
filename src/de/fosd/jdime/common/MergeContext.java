a95ea4d6ace5550547f1066a969428d60cc7e2ff
Georg Seibt <seibt@fim.uni-passau.de>
2016-02-15 15:50:05 +0100

    trim file names after split

--- src/de/fosd/jdime/common/MergeContext.java
+++ src/de/fosd/jdime/common/MergeContext.java
@@ -300,7 +300,8 @@ public class MergeContext implements Cloneable {
             ArtifactList<FileArtifact> inputArtifacts = new ArtifactList<>();
             char cond = 'A';
 
-            for (String fileName : paths) {
+            for (String name : paths) {
+                String fileName = name.trim();
 
                 try {
                     FileArtifact newArtifact = new FileArtifact(new File(fileName));
