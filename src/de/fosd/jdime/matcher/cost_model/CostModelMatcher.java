8f7a7adbebe37873777ba17c7800253ab69f92b4
Georg Seibt <seibt@fim.uni-passau.de>
2016-06-09 23:34:08 +0200

    bound invariantSibling cost, ancestryViolation cost and the matching cost

--- src/de/fosd/jdime/matcher/cost_model/CostModelMatcher.java
+++ src/de/fosd/jdime/matcher/cost_model/CostModelMatcher.java
@@ -1,9 +1,10 @@
 package de.fosd.jdime.matcher.cost_model;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
@@ -25,7 +26,7 @@ public class CostModelMatcher<T extends Artifact<T>> implements MatcherInterface
     @FunctionalInterface
     private interface WeightFunction<T extends Artifact<T>> {
 
-        float weigh(CostModelMatching<T>  matching, int quantity);
+        float weigh(CostModelMatching<T>  matching, float quantity);
     }
 
     private static final class Bounds {
@@ -107,7 +108,15 @@ public class CostModelMatcher<T extends Artifact<T>> implements MatcherInterface
         this.ws = ws;
     }
 
-    private float weighRenaming(CostModelMatching<T> matching) {
+    private Bounds boundMatchingCost(CostModelMatching<T> matching, Collection<CostModelMatching<T>> currentMatchings) {
+        float cR = renamingCost(matching);
+        Bounds cABounds = boundAncestryViolationCost(matching, currentMatchings);
+        Bounds cSBounds = boundSiblingGroupBreakupCost(matching, currentMatchings);
+
+        return new Bounds(cR + cABounds.lower + cSBounds.lower, cR + cABounds.upper + cSBounds.upper);
+    }
+
+    private float renamingCost(CostModelMatching<T> matching) {
         if (matching.isNoMatch() || matching.m.matches(matching.n)) {
             return 0;
         } else {
@@ -115,22 +124,20 @@ public class CostModelMatcher<T extends Artifact<T>> implements MatcherInterface
         }
     }
 
-    private Bounds boundAncestryViolationWeight(CostModelMatching<T> matching, Collection<CostModelMatching<T>> currentMatchings) {
-        Function<Predicate<CostModelMatching<T>>, Boolean> upper = currentMatchings.stream()::anyMatch;
-        Function<Predicate<CostModelMatching<T>>, Boolean> lower = currentMatchings.stream()::noneMatch;
+    private Bounds boundAncestryViolationCost(CostModelMatching<T> matching, Collection<CostModelMatching<T>> currentMatchings) {
         T m = matching.m;
         T n = matching.n;
-
-        Stream<T> mUpper = m.getChildren().stream().filter(mAp -> ancestryIndicator(mAp, n, currentMatchings, true));
-        Stream<T> nUpper = n.getChildren().stream().filter(nAp -> ancestryIndicator(nAp, m, currentMatchings, true));
 
         Stream<T> mLower = m.getChildren().stream().filter(mAp -> ancestryIndicator(mAp, n, currentMatchings, false));
         Stream<T> nLower = n.getChildren().stream().filter(nAp -> ancestryIndicator(nAp, m, currentMatchings, false));
 
-        int upperBound = (int) (mUpper.count() + nUpper.count());
-        int lowerBound = (int) (mLower.count() + nLower.count());
+        Stream<T> mUpper = m.getChildren().stream().filter(mAp -> ancestryIndicator(mAp, n, currentMatchings, true));
+        Stream<T> nUpper = n.getChildren().stream().filter(nAp -> ancestryIndicator(nAp, m, currentMatchings, true));
 
-        return new Bounds(wa.weigh(matching, upperBound), wa.weigh(matching, lowerBound));
+        int lowerBound = (int) (mLower.count() + nLower.count());
+        int upperBound = (int) (mUpper.count() + nUpper.count());
+
+        return new Bounds(wa.weigh(matching, lowerBound), wa.weigh(matching, upperBound));
     }
 
     private boolean ancestryIndicator(T mAp, T n, Collection<CostModelMatching<T>> g, boolean upper) {
@@ -139,6 +146,67 @@ public class CostModelMatcher<T extends Artifact<T>> implements MatcherInterface
             return g.stream().anyMatch(match -> match.m == mAp && !(match.n == null || n.getChildren().contains(match.n)));
         } else {
             return g.stream().noneMatch(match -> match.m == mAp && (match.n == null || n.getChildren().contains(match.n)));
+        }
+    }
+
+    private Bounds boundSiblingGroupBreakupCost(CostModelMatching<T> matching, Collection<CostModelMatching<T>> currentMatchings) {
+        T m = matching.m;
+        T n = matching.n;
+
+        Bounds dMN = boundDistinctSiblingGroups(m, n, currentMatchings);
+        Bounds dNM = boundDistinctSiblingGroups(n, m, currentMatchings);
+
+        Bounds iMN = boundInvariantSiblings(m, n, currentMatchings);
+        Bounds iNM = boundInvariantSiblings(n, m, currentMatchings);
+
+        float lower = ws.weigh(matching, ((dMN.lower / (iMN.upper * (dMN.lower + 1))) + (dNM.lower / (iNM.upper * (dNM.lower + 1)))));
+        float upper = ws.weigh(matching, (dMN.upper / iMN.lower) + (dNM.upper / iNM.lower)) / 2;
+
+        return new Bounds(lower, upper);
+    }
+
+    private Bounds boundDistinctSiblingGroups(T m, T n, Collection<CostModelMatching<T>> currentMatchings) {
+        long lower = m.getChildren().stream().filter(mAp -> distinctSiblingIndicator(mAp, n, currentMatchings, false)).count();
+        long upper = m.getChildren().stream().filter(mAp -> distinctSiblingIndicator(mAp, n, currentMatchings, true)).count();
+
+        return new Bounds(lower, upper);
+    }
+
+    private boolean distinctSiblingIndicator(T mAp, T n, Collection<CostModelMatching<T>> g, boolean upper) {
+
+        if (upper) {
+            return g.stream().anyMatch(match -> match.m == mAp && !(match.n == null || otherSiblings(n).contains(match.n)));
+        } else {
+            return g.stream().noneMatch(match -> match.m == mAp && (match.n == null || otherSiblings(n).contains(match.n)));
+        }
+    }
+
+    private Bounds boundInvariantSiblings(T m, T n, Collection<CostModelMatching<T>> currentMatchings) {
+        long lower = m.getChildren().stream().filter(mAp -> invariantSiblingIndicator(mAp, n, currentMatchings, false)).count();
+        long upper = m.getChildren().stream().filter(mAp -> invariantSiblingIndicator(mAp, n, currentMatchings, true)).count();
+
+        return new Bounds(lower + 1, upper + 1);
+    }
+
+    private boolean invariantSiblingIndicator(T mAp, T n, Collection<CostModelMatching<T>> g, boolean upper) {
+
+        if (upper) {
+            return g.stream().anyMatch(match -> match.m == mAp && otherSiblings(n).contains(match.n));
+        } else {
+            return g.stream().allMatch(match -> (match.m != mAp) || otherSiblings(n).contains(match.n));
+        }
+    }
+
+    private List<T> otherSiblings(T artifact) {
+        T parent = artifact.getParent();
+
+        if (parent == null) {
+            return Collections.emptyList();
+        } else {
+            List<T> res = new ArrayList<T>(parent.getChildren());
+            res.remove(artifact);
+
+            return res;
         }
     }
 
