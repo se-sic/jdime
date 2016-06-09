5ca1e94291ea26bbd7728492b9a08379fcb7148a
Georg Seibt <seibt@fim.uni-passau.de>
2016-06-09 20:14:14 +0200

    add weight functions

--- src/de/fosd/jdime/matcher/cost_model/CostModelMatcher.java
+++ src/de/fosd/jdime/matcher/cost_model/CostModelMatcher.java
@@ -3,9 +3,71 @@ package de.fosd.jdime.matcher.cost_model;
 import de.fosd.jdime.common.Artifact;
 import de.fosd.jdime.common.MergeContext;
 import de.fosd.jdime.matcher.MatcherInterface;
+import de.fosd.jdime.matcher.matching.Matching;
 import de.fosd.jdime.matcher.matching.Matchings;
 
 public class CostModelMatcher<T extends Artifact<T>> implements MatcherInterface<T> {
+
+    @FunctionalInterface
+    private interface SimpleWeightFunction<T extends Artifact<T>> {
+
+        float weigh(Matching<T> matching);
+    }
+
+    @FunctionalInterface
+    private interface WeightFunction<T extends Artifact<T>> {
+
+        float weigh(Matching<T>  matching, int quantity);
+    }
+
+    /**
+     * The cost of not matching an artifact.
+     */
+    private float wn;
+
+    /**
+     * The function determining the cost of renaming an artifact. This cost is 0 if the artifacts match according to
+     * {@link Artifact#matches}.
+     */
+    private SimpleWeightFunction<T> wr;
+
+    /**
+     * The function determining the cost of ancestry violations.
+     */
+    private WeightFunction<T> wa;
+
+    /**
+     * The function determining the cost of breaking up sibling groups.
+     */
+    private WeightFunction<T> ws;
+
+    public void setNoMatchWeight(float wn) {
+        this.wn = wn;
+    }
+
+    public void setRenamingWeight(float wr) {
+        this.wr = matching -> wr;
+    }
+
+    public void setRenamingWeight(SimpleWeightFunction<T> wr) {
+        this.wr = wr;
+    }
+
+    public void setAncestryViolationWeight(float wa) {
+        this.wa = (matching, quantity) -> wa * quantity;
+    }
+
+    public void setAncestryViolationWeight(WeightFunction<T> wa) {
+        this.wa = wa;
+    }
+
+    public void setSiblingGroupBreakupWeight(float ws) {
+        this.ws = (matching, quantity) -> ws * quantity;
+    }
+
+    public void setSiblingGroupBreakupWeight(WeightFunction<T> ws) {
+        this.ws = ws;
+    }
 
     @Override
     public Matchings<T> match(MergeContext context, T left, T right) {
