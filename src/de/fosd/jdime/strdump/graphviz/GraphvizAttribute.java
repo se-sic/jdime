71d9e0c512087297913213cb342940c6f4fa618a
Georg Seibt <seibt@fim.uni-passau.de>
2016-07-07 23:05:16 +0200

    dump Attribute and AttributeList

--- src/de/fosd/jdime/strdump/graphviz/GraphvizAttribute.java
+++ src/de/fosd/jdime/strdump/graphviz/GraphvizAttribute.java
@@ -4,8 +4,16 @@ import java.io.PrintWriter;
 
 public class GraphvizAttribute implements GraphvizElement {
 
+    private final String lhs;
+    private final String rhs;
+
+    public GraphvizAttribute(String lhs, String rhs) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
     @Override
     public void dump(PrintWriter out) {
-
+        out.printf("\"%s\"=\"%s\"", lhs, rhs);
     }
 }
--- src/de/fosd/jdime/strdump/graphviz/GraphvizAttributeList.java
+++ src/de/fosd/jdime/strdump/graphviz/GraphvizAttributeList.java
@@ -1,11 +1,30 @@
 package de.fosd.jdime.strdump.graphviz;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class GraphvizAttributeList implements GraphvizElement {
 
+    private final List<GraphvizAttribute> attributes;
+
+    public GraphvizAttributeList() {
+        this.attributes = new ArrayList<>();
+    }
+
     @Override
     public void dump(PrintWriter out) {
+        out.write('[');
 
+        for (Iterator<GraphvizAttribute> it = attributes.iterator(); it.hasNext();) {
+            it.next().dump(out);
+
+            if (it.hasNext()) {
+                out.write(", ");
+            }
+        }
+
+        out.write(']');
     }
 }
