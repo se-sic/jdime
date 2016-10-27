33c83fadd3cf4154c551080a6506679058e2af4c
Georg Seibt <seibt@fim.uni-passau.de>
2016-10-27 15:34:07 +0200

    move ASTNodeArtifactTest to the correct package

--- test/de/fosd/jdime/artifact/ast/ASTNodeArtifactTest.java
+++ test/de/fosd/jdime/artifact/ast/ASTNodeArtifactTest.java
@@ -21,12 +21,11 @@
  *     Olaf Lessenich <lessenic@fim.uni-passau.de>
  *     Georg Seibt <seibt@fim.uni-passau.de>
  */
-package de.fosd.jdime.artifact;
+package de.fosd.jdime.artifact.ast;
 
 import java.io.File;
 
 import de.fosd.jdime.JDimeTest;
-import de.fosd.jdime.artifact.ast.ASTNodeArtifact;
 import de.fosd.jdime.artifact.file.FileArtifact;
 import de.fosd.jdime.config.JDimeConfig;
 import de.fosd.jdime.config.merge.Revision;
