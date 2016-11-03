c6d1a49e7a2a6bad061a49d7e210c75a401d727f
Georg Seibt <seibt@fim.uni-passau.de>
2016-11-03 16:52:02 +0100

    doc

--- src/de/fosd/jdime/matcher/unordered/IdenticalSubtreeMatcher.java
+++ src/de/fosd/jdime/matcher/unordered/IdenticalSubtreeMatcher.java
@@ -17,16 +17,43 @@ public class IdenticalSubtreeMatcher<T extends Artifact<T>> implements MatcherIn
 
     private Map<T, T> matches;
 
+    /**
+     * Constructs a new {@link IdenticalSubtreeMatcher}.
+     */
     public IdenticalSubtreeMatcher() {
         this.matches = new HashMap<>();
     }
 
+    /**
+     * Stores all matchings resulting from unique matching tree hashes in the left and right trees in this
+     * {@link IdenticalSubtreeMatcher}. After this method was called, the
+     * {@link #match(MergeContext, Artifact, Artifact)} will return {@link Matchings} between the full trees if
+     * {@link #hasMatched(Artifact, Artifact)} returns {@code true}.
+     *
+     * @param leftRoot
+     *         the root of the left tree
+     * @param rightRoot
+     *         the root of the right tree
+     */
     public void matchTrees(T leftRoot, T rightRoot) {
         Map<String, T> leftUnique = uniqueHashes(leftRoot);
         Map<String, T> rightUnique = uniqueHashes(rightRoot);
         matchSubtree(leftUnique.keySet(), leftRoot, rightUnique);
     }
 
+    /**
+     * If the tree hash of {@code left} is unique in the left tree, examines the unique hashes of the right tree and
+     * tries to find the hash of {@code left}. If found, adds matchings between all nodes in the {@code left} and
+     * matching right tree to {@link #matches}. Otherwise all other nodes in the {@code left} tree are examined in DFS
+     * order.
+     *
+     * @param leftUnique
+     *         the unique hashes in the left tree
+     * @param left
+     *         the node from the left tree to be examined
+     * @param rightUnique
+     *         the unique hashes in the right tree and their corresponding nodes
+     */
     private void matchSubtree(Set<String> leftUnique, T left, Map<String, T> rightUnique) {
         String treeHash = left.getTreeHash();
 
@@ -61,10 +88,14 @@ public class IdenticalSubtreeMatcher<T extends Artifact<T>> implements MatcherIn
         return hashes;
     }
 
+    public boolean hasMatched(T left, T right) {
+        return matches.get(left) == right;
+    }
+
     @Override
     public Matchings<T> match(MergeContext context, T left, T right) {
 
-        if (matches.get(left) == right) {
+        if (hasMatched(left, right)) {
             return constructMatchings(new Matchings<>(), left, left.getTreeSize());
         }
 
