a46dc8a869f4f9fa99e4afdab96162bb81650a37
Georg Seibt <seibt@fim.uni-passau.de>
2016-11-24 16:30:49 +0100

    correct expected values after the addition of the BLOCK type

--- test/de/fosd/jdime/stats/StatisticsTest.java
+++ test/de/fosd/jdime/stats/StatisticsTest.java
@@ -30,7 +30,6 @@ import de.fosd.jdime.JDimeTest;
 import de.fosd.jdime.Main;
 import de.fosd.jdime.artifact.ArtifactList;
 import de.fosd.jdime.artifact.file.FileArtifact;
-import de.fosd.jdime.artifact.file.FileArtifact.FileType;
 import de.fosd.jdime.config.merge.MergeContext;
 import de.fosd.jdime.config.merge.MergeScenario;
 import de.fosd.jdime.config.merge.MergeType;
@@ -39,7 +38,6 @@ import de.fosd.jdime.strategy.MergeStrategy;
 import org.junit.Before;
 import org.junit.Test;
 
-import static de.fosd.jdime.artifact.file.FileArtifact.FileType.FILE;
 import static de.fosd.jdime.artifact.file.FileArtifact.FileType.VFILE;
 import static de.fosd.jdime.strategy.MergeStrategy.STRUCTURED;
 import static org.junit.Assert.assertEquals;
@@ -202,10 +200,19 @@ public class StatisticsTest extends JDimeTest {
         }
 
         {
+            ElementStatistics elStats = fileMergeStats.getTypeStatistics(MergeScenario.LEFT, KeyEnums.Type.BLOCK);
+            assertEquals(2, elStats.getTotal());
+            assertEquals(1, elStats.getNumAdded());
+            assertEquals(1, elStats.getNumMerged());
+            assertEquals(0, elStats.getNumDeleted());
+            assertEquals(0, elStats.getNumOccurInConflict());
+        }
+
+        {
             ElementStatistics elStats = fileMergeStats.getTypeStatistics(MergeScenario.LEFT, KeyEnums.Type.NODE);
-            assertEquals(44, elStats.getTotal());
-            assertEquals(12, elStats.getNumAdded());
-            assertEquals(32, elStats.getNumMerged());
+            assertEquals(42, elStats.getTotal());
+            assertEquals(11, elStats.getNumAdded());
+            assertEquals(31, elStats.getNumMerged());
             assertEquals(0, elStats.getNumDeleted());
             assertEquals(0, elStats.getNumOccurInConflict());
         }
@@ -229,10 +236,19 @@ public class StatisticsTest extends JDimeTest {
         }
 
         {
+            ElementStatistics elStats = fileMergeStats.getTypeStatistics(MergeScenario.RIGHT, KeyEnums.Type.BLOCK);
+            assertEquals(2, elStats.getTotal());
+            assertEquals(1, elStats.getNumAdded());
+            assertEquals(1, elStats.getNumMerged());
+            assertEquals(0, elStats.getNumDeleted());
+            assertEquals(0, elStats.getNumOccurInConflict());
+        }
+
+        {
             ElementStatistics elStats = fileMergeStats.getTypeStatistics(MergeScenario.RIGHT, KeyEnums.Type.NODE);
-            assertEquals(45, elStats.getTotal());
-            assertEquals(13, elStats.getNumAdded());
-            assertEquals(32, elStats.getNumMerged());
+            assertEquals(43, elStats.getTotal());
+            assertEquals(12, elStats.getNumAdded());
+            assertEquals(31, elStats.getNumMerged());
             assertEquals(0, elStats.getNumDeleted());
             assertEquals(0, elStats.getNumOccurInConflict());
         }
@@ -254,9 +270,18 @@ public class StatisticsTest extends JDimeTest {
         }
 
         {
+            ElementStatistics elStats = fileMergeStats.getTypeStatistics(MergeScenario.TARGET, KeyEnums.Type.BLOCK);
+            assertEquals(3, elStats.getTotal());
+            assertEquals(2, elStats.getNumAdded());
+            assertEquals(0, elStats.getNumMerged());
+            assertEquals(0, elStats.getNumDeleted());
+            assertEquals(0, elStats.getNumOccurInConflict());
+        }
+
+        {
             ElementStatistics elStats = fileMergeStats.getTypeStatistics(MergeScenario.TARGET, KeyEnums.Type.NODE);
-            assertEquals(57, elStats.getTotal());
-            assertEquals(25, elStats.getNumAdded());
+            assertEquals(54, elStats.getTotal());
+            assertEquals(23, elStats.getNumAdded());
             assertEquals(0, elStats.getNumDeleted());
             assertEquals(0, elStats.getNumOccurInConflict());
         }
