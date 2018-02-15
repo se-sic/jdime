77628b5562e7309b2614399b5297d01afddb1834
Olaf Lessenich <lessenic@fim.uni-passau.de>
2018-02-15 18:08:29 +0100

    REVISE THIS: Clear matches of subtree if nodes are set to be in conflict with each other
    Hypothesis:
    If we already decided for this to be resolved by the developer doing the
    merge, we can throw existing matching information away here as well.
    
    If you don't agree or have use case where we want to use the matching
    information inside of conflicts, revert this commit.
    
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

--- src/de/fosd/jdime/artifact/ast/ASTNodeArtifact.java
+++ src/de/fosd/jdime/artifact/ast/ASTNodeArtifact.java
@@ -24,10 +24,7 @@
 package de.fosd.jdime.artifact.ast;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 import java.util.logging.Level;
