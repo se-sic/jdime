cb49307003244b40d055ce9263f1b43bc01fd415
Olaf Lessenich <lessenic@fim.uni-passau.de>
2018-02-19 08:27:43 +0100

    Provide a way to merge subsequent conflicts as a post-processing step
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

---
+++ src/de/fosd/jdime/util/ConflictOptimizer.java
@@ -0,0 +1,88 @@
+package de.fosd.jdime.util;
+
+import de.fosd.jdime.config.merge.MergeContext;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ConflictOptimizer {
+    private ConflictOptimizer() {}
+
+    private static final Pattern conflictStart = Pattern.compile("^" + MergeContext.conflictStart + ".*");
+    private static final Pattern conflictSep = Pattern.compile("^" + MergeContext.conflictSep);
+    private static final Pattern conflictEnd = Pattern.compile("^" + MergeContext.conflictEnd + ".*");
+
+    private enum Position {
+        NO_CONFLICT, LEFT_SIDE, RIGHT_SIDE, AFTER_CONFLICT
+    }
+
+    /**
+     * Merges subsequent conflicts.
+     *
+     * @param in merge result that should be optimized w.r.t. conflicts
+     * @return optimized merge result
+     */
+    public static String mergeSubsequentConflicts(String in) {
+        Scanner s = new Scanner(in);
+        StringBuilder out = new StringBuilder();
+
+        Position pos = Position.NO_CONFLICT;
+        List<String> left = new LinkedList<>();
+        List<String> right = new LinkedList<>();
+
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+
+            if (matches(conflictStart, line)) {
+
+                pos = Position.LEFT_SIDE;
+            } else if (matches(conflictSep, line)) {
+                pos = Position.RIGHT_SIDE;
+            } else if (matches(conflictEnd, line)) {
+                pos = Position.AFTER_CONFLICT;
+            } else {
+                switch (pos) {
+                    case LEFT_SIDE:
+                        left.add(line + System.lineSeparator());
+                        break;
+                    case RIGHT_SIDE:
+                        right.add(line + System.lineSeparator());
+                        break;
+                    case AFTER_CONFLICT:
+                        pos = Position.NO_CONFLICT;
+                    case NO_CONFLICT:
+                        if (pos == Position.NO_CONFLICT && (!left.isEmpty() || !right.isEmpty())) {
+                            // -> print the previous conflict
+                            out.append(MergeContext.conflictStart + System.lineSeparator());
+                            for (String l : left) { out.append(l); }
+                            out.append(MergeContext.conflictSep + System.lineSeparator());
+                            for (String r : right) { out.append(r); }
+                            out.append(MergeContext.conflictEnd + System.lineSeparator());
+
+                            left.clear();
+                            right.clear();
+                        }
+                        out.append(line + System.lineSeparator());
+                        break;
+                }
+            }
+        }
+
+        return out.toString();
+    }
+
+    /**
+     * Returns whether the given <code>Pattern</code> matches the <code>line</code>.
+     *
+     * @param p
+     *         the <code>Pattern</code> to use
+     * @param line
+     *         the line to match against the pattern
+     * @return true iff the <code>Pattern</code> matched
+     */
+    private static boolean matches(Pattern p, String line) {
+        return p.matcher(line).matches();
+    }
+}
