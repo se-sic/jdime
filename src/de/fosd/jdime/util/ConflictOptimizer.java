dc18cdc06de5cf8ea0dc07fb4fb65aef53f0fa43
Olaf Lessenich <lessenic@fim.uni-passau.de>
2018-02-19 08:51:25 +0100

    Add license header
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

--- src/de/fosd/jdime/util/ConflictOptimizer.java
+++ src/de/fosd/jdime/util/ConflictOptimizer.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright (C) 2013-2014 Olaf Lessenich
+ * Copyright (C) 2014-2017 University of Passau, Germany
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ *
+ * Contributors:
+ *     Olaf Lessenich <lessenic@fim.uni-passau.de>
+ *     Georg Seibt <seibt@fim.uni-passau.de>
+ */
 package de.fosd.jdime.util;
 
 import de.fosd.jdime.config.merge.MergeContext;
@@ -8,7 +31,11 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/**
+ * @author Olaf Lessenich
+ */
 public class ConflictOptimizer {
+
     private ConflictOptimizer() {}
 
     private static final Pattern conflictStart = Pattern.compile("^" + MergeContext.conflictStart + ".*");
