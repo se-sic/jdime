61d7dc136c0611facabdfde4c1ad07dabb5cd085
Olaf Lessenich <lessenic@fim.uni-passau.de>
2018-02-26 16:26:48 +0100

    Update version number to 0.4.3.2
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

--- src/de/fosd/jdime/Main.java
+++ src/de/fosd/jdime/Main.java
@@ -87,7 +87,7 @@ public final class Main {
     }
 
     public static final String TOOLNAME = "jdime";
-    public static final String VERSION = "0.4.3.1";
+    public static final String VERSION = "0.4.3.2";
 
     private static final String MODE_LIST = "list";
 
