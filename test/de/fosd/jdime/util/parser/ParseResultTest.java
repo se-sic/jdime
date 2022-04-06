177ad0216069df65b0ffb0aa4745dddd19296d47
Georg Seibt <seibt@fim.uni-passau.de>
2022-04-06 19:05:01 +0200

    Replace deprecated method

--- test/de/fosd/jdime/util/parser/ParseResultTest.java
+++ test/de/fosd/jdime/util/parser/ParseResultTest.java
@@ -29,9 +29,8 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for <code>ParseResult</code>
